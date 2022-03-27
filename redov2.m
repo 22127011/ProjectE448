640a8dd7de0bcd509b3dbb8bb1f4b249eee6ad58
22127011 <71796091+22127011@users.noreply.github.com>
2022-03-27 08:08:20 +0200

    Add files via upload

--- redov2.m
+++ redov2.m
@@ -1,22 +1,21 @@
 %% Speed control with functions
 
-load('set12el.mat'); % loads matlab sensor data
+load('matlab.mat'); % loads matlab sensor data
 lat = Position.latitude;
 long = Position.longitude; 
 alt = Position.altitude;
 size = length(lat);
-rtimeflat = 20; % completion time in x seconds. Modified by user
-fpc = 15; % frequency of pace change in seconds. Modified by user
+fpc = 15; % frequency of pace change in seconds.
+rtimeflat = 10; % completion time in x seconds. Modified by user
 x = rtimeflat;
 
 [df,avgspeed] = distanceIncr(x,lat,long,size);
 % nww = segmentTimes(x,alt,fpc);
+% nww = [0,10,20,10];
 
 % Distance for every second
-dist = zeros(1);
-for i = 2:1:x+1
-    dist = cat(1, dist, dist(i-1)+avgspeed);
-end
+% dist = diste(df,nww,x,fpc,size);
+dist = distf(x,avgspeed);
 
 % plot 
 myPlot(df,x,lat,long,1,dist,size);

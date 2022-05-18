e9fe1059c698c14e7af028f5a23652020dc2f60f
Thato Matona <71796091+22127011@users.noreply.github.com>
2022-05-18 03:39:22 +0200

    Update with bearing consideration and color on map

--- Extrapolation+its+test/distanceIncr.m
+++ Extrapolation+its+test/distanceIncr.m
@@ -4,15 +4,10 @@ function [df, dInc, NewLatLong] = distanceIncr(lat,long,course,size)
     df = zeros(1);
     dInc = [];
     NewLatLong = [lat(1) long(1)];
-    lat1 = lat(1)*pi/180;
-    long1 = long(1)*pi/180;
-    d_move = 0.2;
+    d_move = 1;
 
     for i=2:1:size
-        % latim1temp = NewLatLong(length(NewLatLong(:,1)),1); % extrapolated previous lat
-        % longim1temp = NewLatLong(length(NewLatLong(:,1)),2); % extrapolated previous long
-
-        latim1temp = lat(i-1);
+        latim1temp = lat(i-1); 
         longim1temp = long(i-1);
 
         a11 = (lat(i)-latim1temp)*pi/180;
@@ -31,16 +26,23 @@ function [df, dInc, NewLatLong] = distanceIncr(lat,long,course,size)
         dInc = cat(1, dInc, d);
 
         br = (course(i))*pi/180;
+        lat1 = latim1temp*pi/180;
+        long1 = longim1temp*pi/180;
         
         dcheck = 0;
-        while(dInc(i-1)>dcheck)
-            lat2 = lat1 + asin(sin(a1)*cos(d_move/R) + cos(a1)*sin(d_move/R)*cos(br));
+        while(dInc(i-1)-dcheck>d_move)
+            lat2 = asin(sin(lat1)*cos(d_move/R) + cos(lat1)*sin(d_move/R)*cos(br));
             long2 = long1 + atan2(sin(br)*sin(d_move/R)*cos(lat1),cos(d_move/R)-sin(lat1)*sin(lat2));
             NewLatLong = cat(1, NewLatLong, [lat2*180/pi long2*180/pi]);
+            y = sin(long2-long1)*cos(lat2);
+            x = cos(lat1)*sin(lat2) - sin(lat1)*cos(lat2)*cos(long2-long1);
+            br = atan2(y, x); % doesnt do much. Only imclude for marks on report
             long1 = long2;
             lat1 = lat2;
             dcheck = dcheck + d_move;
         end
-
+        if (dInc(i-1)-dcheck~=0)
+        NewLatLong = cat(1, NewLatLong, [lat(i) long(i)]);
+        end
      end 
 end
--- Extrapolation+its+test/extrapolation_test.m
+++ Extrapolation+its+test/extrapolation_test.m
@@ -4,19 +4,22 @@ long = Position.longitude;
 alt = Position.altitude;
 course = Position.course;
 size = length(lat);
-st = rateControl(5); % 1/d_move to get 1 second per 1m
+st = rateControl(1); % 1/d_move to get 1 second per 1m
 
 [df dInc nll] = distanceIncr(lat,long,course,size);
 [newdf newdInc] = checknll(nll(:,1),nll(:,2),length(nll(:,1)));
 
-player = geoplayer(lat(1),long(1),18);
-player.Parent.Name = 'Thato vs AI';
+player = geoplayer(lat(1),long(1),21);
+player.Parent.Name = 'RecorderHD';
 player.Basemap = 'streets'; % 'darkwater', 'grayland', 'bluegreen', 'grayterrain', 'colorterrain', 'landcover', 'streets', 'streets-light', 'streets-dark', 'satellite', 'topographic', 'none'
 plotRoute(player,nll(:,1),nll(:,2));
-
+plotRoute(player,lat,long,"Color","blue");
+plotRoute(player,lat(1:110),long(1:110),"Color","yellow");
 for i = 1:length(nll(:,1))
-    plotPosition(player,nll(i,1),nll(i,2),"TrackID",1,"Marker","*","Label","AI");
+    plotPosition(player,nll(i,1),nll(i,2),"TrackID",1,"Marker","*","Label","HD");
     distance = newdf(i)
     time = st.TotalElapsedTime
-    waitfor(st); 
+    %waitfor(st); 
 end
+
+%edit newdInc to suit you
